237322e812faf033335030844204467950155cfc
Glitch (hello-express) <none>
2020-07-01 07:16:24 +0000

    🏑🕹 Checkpoint ./commands/economy/buy.js:7452003/293

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -4,7 +4,12 @@ module.exports = {
   name: "buy",
   run: async(bot, message, args) =>{
     if(args[0] === '1') {
+      let errorEmbed = new Discord.MessageEmbed()
+    .setTitle("**Error!**")
+    .setColor("#FF0000")
+    .setDescription("You have no pocket cash!")
       let pocket = db.fetch(`pocket_${message.author.id}`)
+      if(pocket === null) return message.channel.send(errorEmbed)
       
     }
   }
