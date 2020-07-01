ac8a798bb0ccb68999a2195dcd76f4079da5fd9e
Glitch (hello-express) <none>
2020-07-01 06:53:24 +0000

    🚩🎗 Checkpoint ./commands/economy/store.js:7452003/2263

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -5,8 +5,7 @@ module.exports = {
   run: async(bot, message, args) =>{
     let storeEmbed = new Discord.MessageEmbed()
     .setAuthor('Store', message.author.displayAvatarURL())
-    .setDescription(`To buy an item use the command \`;buy [item]\`.\n\n **Sunglasses - $100**\n Get that dumb sun out of your eyes with these babies!\n\n **Baseball Cap - $500**\n Get that pesky bright star out of your face with this!\n\n **Pizza - $1000**\n Yummy!\n`)
+    .setDescription(`To buy an item use the command \`;buy [item]\`.\n\n **Sunglasses - $100**\n Get that dumb sun out of your eyes with these babies!\n\n **Baseball Cap - $500**\n Get that pesky bright star out of your face with this!\n\n **Pizza - $1000**\n Yummy!\n\n**Axe - $5000**\n Beat your enemies...\n\n**Chainsaw - $10,000**\n I'm not even going to explain this.\n\n**a dad - $69,420**\n dad\n\n**range rover sports truck - $100,000**\n lil yachty\n\n**The Holy "h" Trophy - `)                                                                                                                           
     message.channel.send(storeEmbed)
   }
-} }
-}\
+} 
