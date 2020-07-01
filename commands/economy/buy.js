f56de2d85884243c3e7ec3ca998cb99005b45f39
Glitch (hello-express) <none>
2020-07-01 22:23:51 +0000

    💵✋ Checkpoint ./commands/economy/buy.js:7452003/681

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -8,9 +8,15 @@ module.exports = {
     .setTitle("**Error!**")
     .setColor("#FF0000")
     .setDescription("You have no pocket cash!")
+      let errorEmbed1 = new Discord.MessageEmbed()
+    .setTitle("**Error!**")
+    .setColor("#FF0000")
+    .setDescription("You don't have enough cash to buy this!")
       let pocket = db.fetch(`pocket_${message.author.id}`)
       if(pocket === null) return message.channel.send(errorEmbed)
-      if(pocket !== 100) return message.channel.send(errorEmbed)
+      if(pocket < 100) return message.channel.send(errorEmbed)
+      db.subtract(`pocket_${message.author.id}`, 100)
+      message.channel.send("Oh yeah... :sunglasses:")
     }
   }
 }
