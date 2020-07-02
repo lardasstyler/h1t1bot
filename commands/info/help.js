e8adc10aa1d94d11589efe98a51393e573da83d7
Glitch (hello-express) <none>
2020-07-02 05:34:21 +0000

    🚩📈 Checkpoint ./commands/info/help.js:7452003/2016

--- commands/info/help.js
+++ commands/info/help.js
@@ -2,9 +2,25 @@ const Discord = require('discord.js');
 module.exports = {
   name: "help",
   run: async(bot, message, args) =>{
-    let embed = new Discord.MessageEmbed()
+    if(args[0] === 'economy') {
+      let embed = new Discord.MessageEmbed()
+      .setTitle("Economy Commands")
+      .setDescription("**bal**\nDisplays your balance.\n\n**work**\nGet money to buy stuff.\n\n**dep**\nDeposit the money from your pocket.\n\n**with**\nWithdraw the money from your bank.\n\n**store**\n Find stuff to buy.\n\n**buy**\nBuy stuff from the store.")
+      .setColor("RANDOM")
+      .setTimestamp()
+      message.channel.send(embed)
+    } if(args[0] === 'fun') {
+      let embed = new Discord.MessageEmbed()
+      .setTitle("Fun Commands")
+      .setDescription("**h**\nh")
+      .setColor("RANDOM")
+      .setTimestamp()
+      message.channel.send(embed)
+     } else {
+      let embed = new Discord.MessageEmbed()
     .setAuthor(`Help Menu`, bot.user.displayAvatarURL())
     .setDescription("To learn more, use the command `;help [category]`.\n\n **💰 Economy**\n The servers economy system!\n\n**😂 Fun**\n Fun commands!\n\n**📖 Info**\nInformation.")
     message.channel.send(embed)
+    }
   }
 }
