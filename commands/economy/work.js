fc686e557ae39a3e03959d696cb8a06fd22fb696
lardasstyler <65788240+lardasstyler@users.noreply.github.com>
2020-07-02 04:23:10 -0400

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
+const used = new Map();
+const Duration = require('humanize-duration');
 module.exports = {
   name: "work",
   run: async(bot, message, args) =>{
+    const cooldown = used.get(message.author.id);
+    if(cooldown) {
+      const remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true})
+      return message.channel.send(`${message.author}, you can't use this command for another **${remaining}**!`)
+    } else {
+      used.set(message.author.id, Date.now() + 1000 * 60 * 10);
+      setTimeout(() => { used.delete(message.author.id), 1000 * 60 * 10});
+    }
     var num = Math.floor(Math.random() * 1001)
     let workSentences = [
       `You helped fix a federal fuilding and earned **$${num}**!`,
