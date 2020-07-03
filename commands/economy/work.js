86deee0468bc2cba589794f1e7d567e761f094d1
lardasstyler <65788240+lardasstyler@users.noreply.github.com>
2020-07-03 13:05:17 -0400

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -6,13 +6,6 @@ module.exports = {
   name: "work",
   run: async(bot, message, args) =>{
     const cooldown = used.get(message.author.id);
-    if(cooldown) {
-      const remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true})
-      return message.channel.send(`${message.author}, you can't use this command for another **${remaining}**!`)
-    } else {
-      used.set(message.author.id, Date.now() + 1000 * 60 * 10);
-      setTimeout(() => { used.delete(message.author.id), 1000 * 60 * 10});
-    }
     var num = Math.floor(Math.random() * 1001)
     let workSentences = [
       `You helped fix a federal fuilding and earned **$${num}**!`,
@@ -21,8 +14,19 @@ module.exports = {
       `you are gay (you earned **$${num}**)`
     ]
     let pocket = db.fetch(`pocket_${message.author.id}`)
-    db.add(`pocket_${message.author.id}`, num)
     var workSentencesOutput = workSentences[Math.floor(Math.random() * workSentences.length)];
-    message.channel.send(workSentencesOutput)
+    if(cooldown) {
+      const remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true})
+      return message.channel.send(`${message.author}, you can't use this command for another **${remaining}**!`)
+    } else {
+      used.set(message.author.id, Date.now() + 1000 * 60 * 10);
+      setTimeout(() => { used.delete(message.author.id), 1000 * 60 * 10});
+      db.add(`pocket_${message.author.id}`, num)
+    
+      message.channel.send(workSentencesOutput)
+    }
+    
+    
+    
   }
 }
